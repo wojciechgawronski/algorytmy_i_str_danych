438e81995a204a271a3a862b04b5a438cbd48aff
W. Gawronski <wgawronski.wg@gmail.com>
2024-05-05 10:54:17 +0200

    sortowanie...

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //        kolejka priorytetowa jest w 3 opcjach
+        //    np. sorted pi-que
+        //        do sortowanai i
     }
 }
